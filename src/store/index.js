3088c2e2d0902efaf87b3e73c867f1f80f691a79
joannaberlin <joanna.kot.ok@gmail.com>
2024-01-22 12:33:01 +0100

    Add weather data related modules to store, create global state for example user to use as a default

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import weatherModules from './modules/weather/index.js';
+
+const store = createStore({
+	modules: {
+		weather: weatherModules,
+	},
+	state() {
+		return {
+			userName: 'Bruce',
+		};
+	},
+	getters: {
+		userName(state) {
+			return state.userName;
+		},
+	},
+});
+
+export default store;

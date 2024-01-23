83b552c77ef7973b761102fc014b4640ddee9bb4
joannaberlin <joanna.kot.ok@gmail.com>
2024-01-23 13:24:50 +0100

    Add link cdn for icons, add icons to clouds widget to be shown conditionally based on clouds %

--- src/components/weather/CloudsWidget.vue
+++ src/components/weather/CloudsWidget.vue
@@ -1,13 +1,41 @@
 <template>
 	<BaseCard>
 		<h2>Today's sky in Berlin</h2>
-		<div class="text_wrapper">
-			<h3>
-				<mark>{{ cloudsDesc }}</mark>
-			</h3>
-			<p>
-				<span>{{ clouds }}%</span> Cloudy
-			</p>
+		<div class="text-icon_wrapper">
+			<div class="text_wrapper">
+				<h3>
+					<mark>{{ cloudsDesc }}</mark>
+				</h3>
+				<p>
+					<span>{{ clouds }}%</span>
+				</p>
+			</div>
+			<div class="icon_wrapper">
+				<i
+					v-if="noCloudy"
+					class="em em-sunglasses"
+					aria-role="presentation"
+					aria-label="face with sunglasses"
+				></i>
+				<i
+					v-else-if="littleCloudy"
+					class="em em-smirk"
+					aria-role="presentation"
+					aria-label="smirk face"
+				></i>
+				<i
+					v-else-if="cloudy"
+					class="em em-face_with_rolling_eyes"
+					aria-role="presentation"
+					aria-label="face with rolling eyes"
+				></i>
+				<i
+					v-else
+					class="em em-pleading_face"
+					aria-role="presentation"
+					aria-label="pleading face"
+				></i>
+			</div>
 		</div>
 	</BaseCard>
 </template>
@@ -15,6 +43,28 @@
 <script>
 export default {
 	props: ['clouds', 'cloudsDesc'],
+	data() {
+		return {
+			cloudsPercent: this.clouds,
+		};
+	},
+	computed: {
+		noCloudy() {
+			if (this.cloudsPercent >= 0 && this.cloudsPercent <= 20) {
+				return true;
+			}
+		},
+		littleCloudy() {
+			if (this.cloudsPercent > 20 && this.cloudsPercent < 51) {
+				return true;
+			}
+		},
+		cloudy() {
+			if (this.clouds > 50 && this.clouds < 81) {
+				return true;
+			}
+		},
+	},
 };
 </script>
 
@@ -43,5 +93,14 @@ h3 mark {
 
 .text_wrapper p:nth-child(2) {
 	font-size: 0.9em;
+}
+
+.text-icon_wrapper {
+	display: flex;
+	align-items: flex-start;
+}
+
+i {
+	margin: 1.4em 0 0 1.1em;
 }
 </style>

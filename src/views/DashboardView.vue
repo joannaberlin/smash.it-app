2a38f853c8366953e99b573bc310e753938982fb
joannaberlin <joanna.kot.ok@gmail.com>
2024-01-23 10:11:24 +0100

    Add styles to first widget

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -4,16 +4,20 @@
 		<div class="item">
 			<BaseCard>
 				<h2>Today's weather in Berlin</h2>
-				<p>{{ weatherData.temp }} °C</p>
-				<img
-					:src="
-						'https://openweathermap.org/img/wn/' +
-						`${weatherData.icon}` +
-						'@2x.png'
-					"
-				/>
-				<h2>It feels like</h2>
-				<p>{{ weatherData.feelTemp }} °C</p>
+				<div class="text-icon_wrapper">
+					<div class="text_container">
+						<p>{{ weatherData.temp }} °C</p>
+						<h2>It feels like</h2>
+						<p>{{ weatherData.feelTemp }} °C</p>
+					</div>
+					<img
+						:src="
+							'https://openweathermap.org/img/wn/' +
+							`${weatherData.icon}` +
+							'@2x.png'
+						"
+					/>
+				</div>
 			</BaseCard>
 		</div>
 		<div class="item">
@@ -65,5 +69,23 @@ export default {
 
 .item h2 {
 	font-size: 0.9em;
+}
+
+.text-icon_wrapper {
+	display: flex;
+	justify-content: space-between;
+}
+
+.text-icon_wrapper img {
+	max-width: 200px;
+}
+
+.text_container p:first-child {
+	font-size: 2em;
+	font-weight: bold;
+}
+
+.text_container p:nth-child(3) {
+	font-size: 1.5em;
 }
 </style>

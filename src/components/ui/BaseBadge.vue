4631c0bf8b0fb3f3b4781fcf4b2dd901b42b7e22
joannaberlin <joanna.kot.ok@gmail.com>
2024-01-24 13:00:03 +0100

    Add styles to base badge

--- src/components/ui/BaseBadge.vue
+++ src/components/ui/BaseBadge.vue
@@ -8,4 +8,24 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.badge {
+	color: var(--color-background-dark);
+	border-radius: 30px;
+	padding: 0.5rem 1.5rem;
+	display: inline-block;
+	margin-right: 0.5rem;
+}
+
+.shortDuration {
+	background-color: var(--color-badge-1);
+}
+
+.mediumDuration {
+	background-color: var(--color-badge-2);
+}
+
+.longDuration {
+	background-color: var(--color-badge-3);
+}
+</style>
